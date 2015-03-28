5808e2929969a94a50e1d9c005f787750ec46f8e
rattrapper <galka.dima@gmail.com>
2015-03-28 23:04:53 +0200

    determinant
    first try on c

---
+++ determinant.c
@@ -0,0 +1,68 @@
+//#include <conio.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+const size = sizeof(int);
+int n;
+
+fill(int**);
+det(int**, int);
+print(int**);
+void main()
+{
+	int **matrix;
+	printf("enter n: ");
+	scanf("%d", &n);
+	matrix = malloc(sizeof(int*) * n);
+	//init 
+	for (int i = 0; i < n; i++)
+	{
+		matrix[i] = malloc(size * n);
+	}
+	fill(matrix);
+	print(matrix);
+	printf("det = %d\n", det(matrix, n));
+	return 0;
+}
+fill(int **matrix)
+{
+	srand(time(0));
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			matrix[i][j] = rand() % 11;
+	return 0;
+}
+print(int **matrix)
+{
+	for (int i = 0; i < n; i++, printf("\r\n"))
+		for (int j = 0; j < n; j++)
+			printf("%d\t", matrix[i][j]);
+}
+int** minor(int** matrix, int row, int n)
+{
+	int **res = malloc(sizeof(int*) * (n - 1));
+	for (int i = 0; i < n; i++)
+	{
+		if (i == row)
+			continue;
+		int k = i < row ? i : i - 1;
+		res[k] = matrix[i] + 1;
+	}
+	return res;
+}
+parity(x)
+{
+	return x % 2 ? -1 : 1;
+}
+int det(int **matrix, int n)
+{
+	if (n == 1)
+		return matrix[0][0];
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+		sum += parity(i) * (matrix[i][0]) * det(minor(matrix, i, n), n - 1);
+	return sum;
+}
